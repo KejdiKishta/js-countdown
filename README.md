b0be710c9d0a9e7857192eb76036a91fadedfbcc
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-04-10 15:17:19 +0200

    add: function for the countdown and interval

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Iniziate stampando il countdown nella console e dopo stampatelo in pagina.
 
 - [x] creo un console log con gli auguri
 - [x] gli imposto un timer di 10 secondi
+- [x] creo una varibile con il numero da cui inizia il countdown
+- [x] creo una funzione che decrementa il numero fino all'1
+- [x] metto un intervallo di un secondo tra un decremento e l'altro
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,19 @@ function congrats() {
 
 //* gli do un timeout di 10 secondi
 setTimeout (congrats, 10000);
+
+//* variabile con i secondi di countdoun
+let countStart = 10;
+console.log(countStart);
+
+//* funzione con i numeri del countdown
+function countdown () {
+    //* se il numero è maggiore di 1 decremento e stampo
+    if (countStart > 1) {
+        countStart--;
+        console.log(countStart);
+    }
+}
+
+//* timeout per il countdown
+setInterval (countdown, 1000);
